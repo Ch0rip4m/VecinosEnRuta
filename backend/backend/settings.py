a2efe667fa90f830a58a32a7a95730ab481c28a2
Ch0rip4m <giorgio.nicolini.a@gmail.com>
2024-11-13 19:01:02 -0300

    ajustes de config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(u3b1$%9bf)$w@30+_p6dgr)2pvudpj#jxbr!*we0*qgvbqvjf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["bevr.robleaustral.cl"]
 
 
 # Application definition
