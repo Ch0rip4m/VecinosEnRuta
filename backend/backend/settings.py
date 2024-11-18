ac7b9c258ef2afaad0aa895a45f12acce7f11127
Ch0rip4m <giorgio.nicolini.a@gmail.com>
2024-11-18 17:57:12 -0300

    rutas actualizado

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,9 +145,9 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000"  # Agrega aquí la URL de tu aplicación frontend
+    "http://bevr.robleaustral.cl"  # Agrega aquí la URL de tu aplicación frontend
 ]
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
+CSRF_TRUSTED_ORIGINS = ["http://bevr.robleaustral.cl"]
 
 # CSRF_COOKIE_NAME = 'csrftoken'
 # CSRF_HEADER_NAME = 'X-Csrftoken'
